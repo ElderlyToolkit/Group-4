32036ddc3fbea3f128cecc715856c19c828fe8f3
Kryptoxic <ryanchew96@gmail.com>
2015-11-27 15:28:09 +0800

    Chatbox draft 1

--- Group 4 Project/src/RyanUIPrototype/Chatbox.java
+++ Group 4 Project/src/RyanUIPrototype/Chatbox.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
 
 public class Chatbox extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -35,8 +44,33 @@ public class Chatbox extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblwelcomeToThe = new JLabel("~Welcome to the chat centre~");
+		lblwelcomeToThe.setBounds(145, 11, 156, 14);
+		contentPane.add(lblwelcomeToThe);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 32, 414, 195);
+		contentPane.add(textArea);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 238, 295, 18);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(367, 236, 57, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.setBounds(307, 236, 60, 23);
+		contentPane.add(btnClear);
 	}
-
 }
