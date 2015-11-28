24e1a94aeba021e649c67f776db55a92793fda37
Kryptoxic <ryanchew96@gmail.com>
2015-11-28 13:44:23 +0800

    Matchmaking draft

--- Group 4 Project/src/RyanUIPrototype/Matchmaking.java
+++ Group 4 Project/src/RyanUIPrototype/Matchmaking.java
@@ -6,6 +6,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
 
 public class Matchmaking extends JFrame {
 
@@ -35,8 +38,29 @@ public class Matchmaking extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblPictureOfUser = new JLabel("Picture of user");
+		lblPictureOfUser.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPictureOfUser.setBounds(10, 11, 93, 65);
+		contentPane.add(lblPictureOfUser);
+		
+		JLabel lblProfile = new JLabel("Profile");
+		lblProfile.setHorizontalAlignment(SwingConstants.CENTER);
+		lblProfile.setBounds(143, 36, 270, 199);
+		contentPane.add(lblProfile);
+		
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setBounds(14, 143, 89, 23);
+		contentPane.add(btnSubmit);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.setBounds(14, 177, 89, 23);
+		contentPane.add(btnClear);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(14, 211, 89, 23);
+		contentPane.add(btnBack);
 	}
-
 }
