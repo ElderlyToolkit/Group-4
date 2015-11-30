028a65e4746a80026b230c2dfb156b7b877fc9a3
Kryptoxic <ryanchew96@gmail.com>
2015-11-30 11:27:01 +0800

    UI updates

--- Group 4 Project/src/RyanUIPrototype/Chatbox.java
+++ Group 4 Project/src/RyanUIPrototype/Chatbox.java
@@ -41,6 +41,7 @@ public class Chatbox extends JFrame {
 	 * Create the frame.
 	 */
 	public Chatbox() {
+		setTitle("Chatroom");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
