7f061e79c599e910a93786a033d441a2ccfaa396
daryl <ithinkmynameisdaryl@gmail.com>
2015-12-10 14:24:28 +0800

    hi

--- Group 4 Project/src/DarylUI/Dictionary.java
+++ Group 4 Project/src/DarylUI/Dictionary.java
@@ -41,6 +41,7 @@ public class Dictionary extends JFrame {
 	 * Create the frame.
 	 */
 	public Dictionary() {
+		setTitle("Dictionary");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
