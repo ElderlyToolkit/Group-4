ddacf4899267e525ae327a066fb70a3d6e8dc44b
peter_000 <peter_000@Cephas.home>
2016-02-01 12:36:30 +0800

    wefwef

---
+++ Group 4 Project/src/Entity/eBooksConstructor.java
@@ -0,0 +1,17 @@
+package Entity;
+
+public class eBooksConstructor {
+	private String category;
+	
+	public eBooksConstructor(String category){
+		this.category = category;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
